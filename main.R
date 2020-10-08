405ed387da1f52d3890331d12154f905b9f9cf84
Kyongho Son <kyonghoson@gmail.com>
2020-10-08 11:38:09 -0700

    Update main.R
    This main scrip includes all R scripts to prepare for the model calibration and run SWAT simulations, and conduct simple calibration/sensitivity analysis/uncertainty analysis

--- main.R
+++ main.R
@@ -1,2 +1,20 @@
 
 # this script is designed for calibration/sensitivity/uncertainty analysis in SWAT modeling
+
+# open SWAT_Cal.R
+# modifying the folder number and file.cio file 
+# basic R functions
+source("Rfunction.R")
+# make sure that the modified parameter values should be witin the phyiscal ranges
+source("checking_param_ranges")
+
+# modifying parameter R scripts
+source("ModifyInputFile.R")
+
+# generating the calibrated model parameter sets
+# and running SWAT simulations
+
+source("SWAT_CalbR.R")
+
+# model calibration/sensitivity analysis/uncertainty analysis
+source("calb_sense_uncertainty.R")
